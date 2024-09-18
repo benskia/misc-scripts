7e36ca443bcfb2bf9a88af6fdf3928b7a9f55cd1
benskia <benski.abraham@gmail.com>
2024-09-18 16:54:35 -0500

    first commit

---
+++ toggle-charge.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/bash
+
+isRoot () {
+    if [[ "$EUID" -ne 0 ]]; then
+        echo "Run as root to change charge thresholds (sudo ./toggle-charge.sh)"
+        exit 1
+    fi
+}
+
+while getopts m: flag
+do
+    case "${flag}" in
+        m) mode=${OPTARG};;
+    esac
+done
+
+if [[ $mode == "ls" ]]; then
+    chargeStart=`cat /sys/class/power_supply/BAT0/charge_control_start_threshold`
+    chargeEnd=`cat /sys/class/power_supply/BAT0/charge_control_end_threshold`
+    chargeStatus=`cat /sys/class/power_supply/BAT0/status`
+
+    echo "Charge start: $chargeStart"
+    echo "Charge end: $chargeEnd"
+    echo "Charge status: $chargeStatus"
+    exit 0
+
+elif [[ $mode == "health" ]]; then
+    fullEnergyDesign=`cat /sys/class/power_supply/BAT0/energy_full_design`
+    fullEnergyActual=`cat /sys/class/power_supply/BAT0/energy_full`
+
+    percentCapacity=`echo "$fullEnergyActual / $fullEnergyDesign * 100" | bc -l`
+
+    echo "Percent capacity: $percentCapacity"
+    exit 0
+
+elif [[ $mode == "mid" ]]; then
+    isRoot
+    midStart=45
+    midEnd=50
+
+    echo "Setting charge start to $midStart"
+    echo $midStart | tee /sys/class/power_supply/BAT0/charge_control_start_threshold
+    echo "Setting charge end to $midEnd"
+    echo $midEnd | tee /sys/class/power_supply/BAT0/charge_control_end_threshold
+    exit 0
+
+elif [[ $mode == "max" ]]; then
+    isRoot
+    maxStart=80
+    maxEnd=90
+
+    echo "Setting charge start to $maxStart"
+    echo $maxStart | tee /sys/class/power_supply/BAT0/charge_control_start_threshold
+    echo "Setting charge end to $maxEnd"
+    echo $maxEnd | tee /sys/class/power_supply/BAT0/charge_control_end_threshold
+    exit 0
+fi
+
+echo "Invalid or missing arg"
+exit 1
